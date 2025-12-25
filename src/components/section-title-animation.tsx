55cc1c976ee12771e66ca9af43ee8245bfbb88b9
Andrej Jurisic <andrejjurisic99@gmail.com>
2025-12-25 14:25:56 +0100

    create section title animation component

---
+++ src/components/section-title-animation.tsx
@@ -0,0 +1,40 @@
+import { cn } from "@/lib/utils";
+import { motion } from "motion/react";
+import FadeInWrapper from "./fade-in-wrapper";
+
+type CubeSectionProps = {
+  src: string;
+  text: string;
+  direction?: "left" | "right";
+};
+
+const SectionTitleAnimation = ({
+  src,
+  text,
+  direction = "left",
+}: CubeSectionProps) => {
+  return (
+    <div className="relative flex min-h-32 items-center justify-center w-full">
+      <motion.img
+        initial={{ x: direction === "left" ? -300 : 300, opacity: 0 }}
+        whileInView={{ x: 0, opacity: 1 }}
+        viewport={{ once: true, margin: "-175px" }}
+        transition={{
+          duration: 0.8,
+          ease: [0.16, 1, 0.3, 1],
+        }}
+        src={src}
+        className={cn(
+          "absolute top-0 h-full w-auto",
+          direction === "left" ? "left-0" : "right-0"
+        )}
+        alt="Decorative cubes"
+      />
+      <FadeInWrapper className="z-10 flex h-full items-center justify-center px-4">
+        <h2 className="text-center text-6xl font-bold text-balance">{text}</h2>
+      </FadeInWrapper>
+    </div>
+  );
+};
+
+export default SectionTitleAnimation;
