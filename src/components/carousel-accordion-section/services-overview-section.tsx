7af0b192b0021d1a8a89309b5a91e0388f6cada2
AndrejJurisic <124591321+AndrejJurisic@users.noreply.github.com>
2025-12-29 12:38:07 +0100

    CL-67 / Services overview section (#8)
    * Enhance CardsCarousel functionality with hover state and adjust interval logic
    
    * install shadcn accordion
    
    * add svgs
    
    * Add ServicesAccordion component for service overview section
    
    * Add ServicesOverviewSection component with animated title and accordion
    
    * Add ServicesOverviewSection to index page
    
    * Wrap ServicesAccordion in FadeInWrapper for improved animation
    
    * refactor code

---
+++ src/components/carousel-accordion-section/services-overview-section.tsx
@@ -0,0 +1,23 @@
+import FadeInWrapper from "../fade-in-wrapper";
+import SectionTitleAnimation from "../section-title-animation";
+import ServicesAccordion from "./services-accordion";
+
+const ServicesOverviewSection = () => {
+  return (
+    <div className="space-y-8 lg:hidden">
+      <SectionTitleAnimation
+        src="/src/svgs/cubes/cubes-right.svg"
+        text="Services Overview"
+      />
+      <FadeInWrapper className="flex w-full items-center justify-center">
+        <h3 className="max-w-72 text-center text-base italic">
+          Comprehensive e-learning solutions to transform your training and
+          development programs.
+        </h3>
+      </FadeInWrapper>
+      <ServicesAccordion />
+    </div>
+  );
+};
+
+export default ServicesOverviewSection;
