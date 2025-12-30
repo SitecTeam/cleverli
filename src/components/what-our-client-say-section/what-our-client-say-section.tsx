b4a061fd15eb781d736a24ba38957df0a9a47f3c
Andrej Jurisic <andrejjurisic99@gmail.com>
2025-12-30 09:30:44 +0100

    Add What Our Clients Say section with title animation and comments carousel

---
+++ src/components/what-our-client-say-section/what-our-client-say-section.tsx
@@ -0,0 +1,13 @@
+import SectionTitleAnimation from "../section-title-animation";
+import ClientCommentsCarousel from "./client-comments-carousel";
+
+const WhatOurClientSaySection = () => {
+  return (
+    <div className="space-y-10 lg:space-y-18">
+      <SectionTitleAnimation text="What Our Clients Say" direction="right" />
+      <ClientCommentsCarousel />
+    </div>
+  );
+};
+
+export default WhatOurClientSaySection;
