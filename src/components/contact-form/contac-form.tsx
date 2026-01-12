54850f24dd381fcf3347e229d1f63868be964e76
Andrej Jurisic <andrejjurisic99@gmail.com>
2026-01-12 10:29:07 +0100

    optimize components for mobile screens

--- src/components/contact-form/contac-form.tsx
+++ src/components/contact-form/contac-form.tsx
@@ -12,19 +12,20 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Textarea } from "../ui/textarea";
 
 const formSchema = z.object({
   name: z.string().min(2, {
-    message: "Name must be at least 2 characters.",
+    message: "Min. 2 characters",
   }),
   email: z.string().email({
-    message: "Please enter a valid email address.",
+    message: "Invalid email",
   }),
   date: z.string().min(1, {
-    message: "Please select a valid date.",
+    message: "Invalid date",
   }),
   time: z.string().min(1, {
-    message: "Please select a valid time.",
+    message: "Invalid time",
   }),
   message: z.string().optional(),
 });
@@ -48,28 +49,24 @@ const ContactForm = () => {
   }
 
   return (
-    <div className="w-full p-7.5 rounded-2xl bg-transparent shadow-box h-full flex flex-col">
-      <h2 className="text-3xl font-bold text-center text-white mb-12">
+    <div className="w-full p-5 sm:p-7.5 rounded-2xl bg-transparent shadow-box h-full flex flex-col">
+      <h2 className="text-xl sm:text-3xl font-bold text-center text-white mb-6 sm:mb-12">
         Schedule your free 30-minute consultation today.
       </h2>
 
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
-          className="flex flex-col flex-1 gap-4"
+          className="flex flex-col flex-1 gap-2 sm:gap-4"
         >
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-7">
+          <div className="grid grid-cols-2 gap-7">
             <FormField
               control={form.control}
               name="name"
               render={({ field }) => (
                 <FormItem>
                   <FormControl>
-                    <Input
-                      placeholder="Name"
-                      {...field}
-                      className="border-0 border-b border-white/20 rounded-none px-0 bg-transparent focus-visible:ring-0 focus-visible:border-orange-400 text-white h-auto py-2"
-                    />
+                    <Input placeholder="Name" {...field} />
                   </FormControl>
                   {errors.name ? (
                     <FormMessage />
@@ -85,11 +82,7 @@ const ContactForm = () => {
               render={({ field }) => (
                 <FormItem>
                   <FormControl>
-                    <Input
-                      placeholder="Email"
-                      {...field}
-                      className="border-0 border-b border-white/20 rounded-none px-0 bg-transparent focus-visible:ring-0 focus-visible:border-orange-400 text-white h-auto py-2"
-                    />
+                    <Input placeholder="Email" {...field} />
                   </FormControl>
                   {errors.email ? (
                     <FormMessage />
@@ -101,7 +94,7 @@ const ContactForm = () => {
             />
           </div>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <div className="grid grid-cols-2 gap-7">
             <FormField
               control={form.control}
               name="date"
@@ -109,11 +102,7 @@ const ContactForm = () => {
                 <FormItem>
                   <FormControl>
                     <div className="relative">
-                      <Input
-                        placeholder="Date"
-                        {...field}
-                        className="border-0 border-b border-white/20 rounded-none px-0 bg-transparent focus-visible:ring-0 focus-visible:border-orange-400 text-white h-auto py-2 pr-10"
-                      />
+                      <Input placeholder="Date" {...field} className="pr-10" />
                       <CalendarDays className="absolute right-0 bottom-0.5 shrink-0 h-10 w-9 text-primary" />
                     </div>
                   </FormControl>
@@ -132,11 +121,7 @@ const ContactForm = () => {
                 <FormItem>
                   <FormControl>
                     <div className="relative">
-                      <Input
-                        placeholder="Time"
-                        {...field}
-                        className="border-0 border-b border-white/20 rounded-none px-0 bg-transparent focus-visible:ring-0 focus-visible:border-orange-400 text-white h-auto py-2 pr-10"
-                      />
+                      <Input placeholder="Time" {...field} className="pr-10" />
                       <Clock className="absolute right-0 bottom-0.5 shrink-0 h-10 w-9 text-primary" />
                     </div>
                   </FormControl>
@@ -156,10 +141,9 @@ const ContactForm = () => {
             render={({ field }) => (
               <FormItem>
                 <FormControl>
-                  <textarea
-                    placeholder="What would you like to discuss?"
+                  <Textarea
                     {...field}
-                    className="flex w-full  border-0 border-b border-white/20 bg-transparent px-0 py-2 text-sm ring-offset-background placeholder:text-[#8596AB] focus-visible:outline-none focus-visible:border-orange-400 disabled:cursor-not-allowed disabled:opacity-50 text-white min-h-[80px] resize-none"
+                    placeholder="What would you like to discuss?"
                   />
                 </FormControl>
                 <FormMessage />
@@ -170,7 +154,7 @@ const ContactForm = () => {
           <Button
             type="submit"
             variant="primary"
-            className="w-full py-6 text-lg rounded-xl transition-all duration-300 mt-auto"
+            className="w-full py-4 sm:py-6 text-base sm:text-lg rounded-xl transition-all duration-300 mt-2 sm:mt-auto"
           >
             Book Your Call Now
           </Button>
--- src/components/scroll-curtain-section.tsx
+++ src/components/scroll-curtain-section.tsx
@@ -51,62 +51,80 @@ const ScrollCurtainSection = () => {
   return (
     <section
       ref={sectionRef}
-      className="relative min-h-[200vh] w-screen left-1/2 -ml-[50vw] -mr-[50vw] snap-start z-0"
+      className="relative min-h-screen lg:min-h-[200vh] w-screen left-1/2 -ml-[50vw] -mr-[50vw] snap-start z-0 mt-10 lg:mt-0"
     >
-      <div className="sticky top-0 h-screen -mt-80">
+      <div className="relative h-full lg:sticky lg:top-0 lg:h-screen lg:-mt-80">
         <motion.div
-          className="absolute z-20 inset-x-0 bottom-0 bg-linear-to-t from-[#2E3642] to-[#303844]"
+          className="relative lg:absolute z-20 inset-x-0 bottom-0 bg-linear-to-t from-[#2E3642] to-[#303844] max-[1024px]:h-full!"
           style={{ height: curtainHeight }}
         >
           <motion.div
-            className="flex flex-col items-center justify-center h-full text-white max-w-360 mx-auto transition ease-in-out"
+            className="flex flex-col items-center justify-center h-full text-white max-w-360 mx-auto transition ease-in-out pt-11 pb-8.5 lg:py-0 max-[1024px]:opacity-100! max-[1024px]:transform-none!"
             style={{
               opacity: contentOpacity,
               y: contentY,
               scale: contentScale,
             }}
           >
-            <h2 className="text-6xl font-bold mb-25">
+            <h2 className="text-3xl sm:text-4xl lg:text-6xl font-bold mb-7 lg:mb-25 text-center text-balance">
               Ready to Transform Your Training?
             </h2>
-            <div className="flex lg:px-10 2xl:px-25 lg:gap-10 2xl:gap-20">
-              <div className="space-y-9.5 flex-1">
-                <p className="lg:text-4xl italic">
+            <div className="flex flex-col-reverse lg:px-10 2xl:px-25 lg:gap-10 2xl:gap-20 lg:flex-row">
+              <div className="space-y-6 lg:space-y-9.5 flex-1">
+                <p className="sm:text-4xl italic pl-5 pr-10 lg:px-0">
                   Let’s build smarter learning together. Contact us today to
                   schedule your free consultation.
                 </p>
-                <div className="space-y-3 lg:text-xl">
-                  <div className="flex gap-8">
-                    <img src="/src/svgs/form/email.svg" alt="Email" />
-                    <div className="flex flex-col gap-1.5">
-                      <span className="text-[#8596AB] ">Email</span>
-                      <span className="font-medium">info@cleverli.pro</span>
+                <div className="space-y-3 sm:text-xl px-10.5 lg:px-0">
+                  <div className="flex gap-5.5 sm:gap-8">
+                    <div className="w-7.5 h-10.5 sm:h-auto sm:w-auto">
+                      <img src="/src/svgs/form/email.svg" alt="Email" />
+                    </div>
+                    <div className="flex flex-col sm:gap-1.5">
+                      <span className="text-[#8596AB] text-sm sm:text-base">
+                        Email
+                      </span>
+                      <span className="font-medium text-sm sm:text-base">
+                        info@cleverli.pro
+                      </span>
                     </div>
                   </div>
-                  <div className="flex gap-8 mt-3.5">
-                    <img src="/src/svgs/form/phone.svg" alt="Phone" />
-                    <div className="flex flex-col gap-1.5">
-                      <span className="text-[#8596AB]">Phone</span>
-                      <div className="flex flex-col gap-0.2 font-medium">
+                  <div className="flex gap-5.5 sm:gap-8">
+                    <div className="w-8 h-11 sm:h-auto sm:w-auto mt-3 sm:mt-0">
+                      <img src="/src/svgs/form/phone.svg" alt="Phone" />
+                    </div>
+                    <div className="flex flex-col sm:gap-1.5">
+                      <span className="text-[#8596AB] text-sm sm:text-base">
+                        Phone
+                      </span>
+                      <div className="flex flex-col gap-0.2 font-medium text-sm sm:text-base">
                         <span>+381 65 323 77 46</span>
                         <span>+387 65 371 839</span>
                         <span>+387 65 746 939</span>
                       </div>
                     </div>
                   </div>
-                  <div className="flex gap-8">
-                    <img src="/src/svgs/form/location.svg" alt="Location" />
-                    <div className="flex flex-col gap-1.5">
-                      <span className="text-[#8596AB]">Location</span>
-                      <span className="font-medium">
+                  <div className="flex gap-5.5 sm:gap-8">
+                    <div className="w-9 h-12 sm:h-auto sm:w-auto">
+                      <img src="/src/svgs/form/location.svg" alt="Location" />
+                    </div>
+                    <div className="flex flex-col sm:gap-1.5">
+                      <span className="text-[#8596AB] text-sm sm:text-base">
+                        Location
+                      </span>
+                      <span className="font-medium text-sm sm:text-base">
                         Banja Luka, Bosnia and Herzegovina
                       </span>
                     </div>
                   </div>
                 </div>
-                <img src="/src/svgs/form/linked-in.svg" alt="LinkedIn" />
+                <img
+                  src="/src/svgs/form/linked-in.svg"
+                  alt="LinkedIn"
+                  className="px-5 lg:px-0"
+                />
               </div>
-              <div className="flex-1">
+              <div className="flex-1 px-5 lg:p-0 pb-8.5">
                 <ContactForm />
               </div>
             </div>
