afc004681241f1ead8458cfce32896d447549594
Andrej Jurisic <andrejjurisic99@gmail.com>
2026-01-15 15:49:22 +0100

    add Our Service Process section to services page

--- src/components/our-service-process-section/our-service-process-section.tsx
+++ src/components/our-service-process-section/our-service-process-section.tsx
@@ -1,3 +1,4 @@
+import FadeInWrapper from "../fade-in-wrapper";
 import SectionTitleAnimation from "../section-title-animation";
 import ServiceProcessCard from "./service-process-card";
 import { serviceProcessData } from "./service-process-data";
@@ -6,11 +7,11 @@ const OurServiceProcessSection = () => {
   return (
     <div className="flex flex-col gap-13">
       <SectionTitleAnimation text="Our Service Process" />
-      <p className="mx-auto max-w-140 text-center text-balance lg:text-xl">
+      <FadeInWrapper className="mx-auto max-w-140 text-center text-balance lg:text-xl">
         We follow a structured approach to deliver high-quality e-learning
         solutions that meet your specific needs.
-      </p>
-      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4 2xl:px-17.5">
+      </FadeInWrapper>
+      <FadeInWrapper className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4 2xl:px-17.5">
         {serviceProcessData.map((item, index) => (
           <ServiceProcessCard
             key={item.title}
@@ -20,7 +21,7 @@ const OurServiceProcessSection = () => {
             index={index + 1}
           />
         ))}
-      </div>
+      </FadeInWrapper>
     </div>
   );
 };
