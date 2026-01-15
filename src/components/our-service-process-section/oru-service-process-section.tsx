5a28f0a0c611e4dd800f786f8d3e57d298420cc6
Andrej Jurisic <andrejjurisic99@gmail.com>
2026-01-15 15:36:55 +0100

    add Our Service Process section with service process cards and data

---
+++ src/components/our-service-process-section/oru-service-process-section.tsx
@@ -0,0 +1,28 @@
+import SectionTitleAnimation from "../section-title-animation";
+import ServiceProcessCard from "./service-process-card";
+import { serviceProcessData } from "./service-process-data";
+
+const OurServiceProcessSection = () => {
+  return (
+    <div className="flex flex-col gap-13">
+      <SectionTitleAnimation text="Our Service Process" />
+      <p className="mx-auto max-w-140 text-center text-balance lg:text-xl">
+        We follow a structured approach to deliver high-quality e-learning
+        solutions that meet your specific needs.
+      </p>
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4 2xl:px-17.5">
+        {serviceProcessData.map((item, index) => (
+          <ServiceProcessCard
+            key={item.title}
+            title={item.title}
+            description={item.description}
+            icon={item.icon}
+            index={index + 1}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default OurServiceProcessSection;
---
+++ src/components/our-service-process-section/service-process-card.tsx
@@ -0,0 +1,71 @@
+import { Card, CardContent } from "../ui/card";
+
+interface ServiceProcessCardProps {
+  title: string;
+  description: string;
+  icon: string;
+  index: number;
+}
+
+const ServiceProcessCard = ({
+  title,
+  description,
+  icon,
+  index,
+}: ServiceProcessCardProps) => {
+  return (
+    <Card
+      className="min-h-75 bg-white/20 pt-2"
+      style={{ boxShadow: "var(--shadow-card)" }}
+    >
+      <CardContent className="flex flex-col px-2">
+        <div className="relative flex flex-col items-center justify-between sm:flex-row sm:items-start">
+          <div className="h-22 sm:contents">
+            <img src={icon} alt={title} className="hidden h-28 sm:block" />
+          </div>
+
+          <div className="absolute -top-3 left-1/2 -translate-x-1/2 bg-clip-text text-8xl font-bold sm:-top-3.5 sm:-right-6 sm:left-auto sm:text-[110px] xl:right-9">
+            {index}
+          </div>
+          <h1
+            className="absolute -top-3 left-1/2 -translate-x-5 bg-clip-text text-8xl font-bold text-black/10 sm:-top-3.5 sm:right-0 sm:left-auto sm:translate-x-0 sm:text-[110px] xl:right-15"
+            style={{
+              WebkitTextStroke: "0.5px white",
+            }}
+          >
+            {index}
+          </h1>
+          <h1
+            className="absolute -top-3 left-1/2 -translate-x-5 text-8xl font-bold text-transparent sm:-top-3.5 sm:right-0 sm:left-auto sm:translate-x-0 sm:text-[110px] xl:right-15"
+            style={{
+              backdropFilter: "blur(3px)",
+              WebkitMask: "linear-gradient(#000 0 0) text",
+              mask: "linear-gradient(#000 0 0) text",
+              WebkitMaskComposite: "source-in",
+              maskComposite: "intersect",
+            }}
+          >
+            {index}
+          </h1>
+        </div>
+        <div className="flex justify-center">
+          <div className="relative">
+            <img
+              src={icon}
+              alt={title}
+              className="absolute -left-12 h-20 min-[320px]:-left-14 sm:hidden"
+            />
+            <h3 className="max-w-40 text-center text-xl font-bold sm:max-w-none">
+              {title}
+            </h3>
+          </div>
+        </div>
+        <p className="mt-4 px-7 text-center text-muted-foreground sm:px-2">
+          {description}
+        </p>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ServiceProcessCard;
