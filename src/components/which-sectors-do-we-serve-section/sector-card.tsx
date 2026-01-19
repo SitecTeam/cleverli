6a6f82542154630c185d260a2a60a2199527653b
AndrejJurisic <124591321+AndrejJurisic@users.noreply.github.com>
2026-01-19 08:48:46 +0100

    CL-71 / Which sectors do we serve section (#22)
    * update fotter and dark panel components
    
    * add svgs
    
    * add sector card data
    
    * add SectorCard component for displaying sector information
    
    * add WhichSectorsDoWeServeSection component and integrate it into sectors page
    
    * add platform svg and implement it
    
    * update logo size in footer for improved responsiveness
    
    * fix: correct typo in buttonLink prop in DarkPanel component usage
    
    * fix typo

---
+++ src/components/which-sectors-do-we-serve-section/sector-card.tsx
@@ -0,0 +1,73 @@
+import { Card, CardContent } from "../ui/card";
+import DetailsIcon from "../../svgs/carousel/details-icon.svg?react";
+import CubeIcon from "../../svgs/accordion/details-cube.svg?react";
+import PlatformIcon from "../../svgs/sector/platform.svg?react";
+
+type SectorCardProps = {
+  title: string;
+  description: string;
+  icon: string;
+  bgIcon: string;
+  details: string[];
+};
+
+const SectorCard = ({
+  title,
+  description,
+  icon,
+  bgIcon,
+  details,
+}: SectorCardProps) => {
+  return (
+    <Card
+      className="relative min-h-92.5 border-none bg-linear-to-t from-[#F8F7F8] via-[#D5DBE2] to-[#B0BBC9] pt-2 pb-2.5 min-[1430px]:h-75 min-[1430px]:min-h-max lg:bg-linear-to-l lg:py-5"
+      style={{ boxShadow: "var(--shadow-card)" }}
+    >
+      <img
+        src={bgIcon}
+        alt={`${title} background`}
+        className="absolute bottom-0 left-1/2 z-0 h-auto max-h-57.5 w-full -translate-x-1/2 px-7 blur-[2px] lg:right-0 lg:left-auto lg:h-full lg:max-h-full lg:w-auto lg:translate-x-0 lg:px-1 lg:py-2"
+      />
+      <CardContent className="z-10 flex h-full flex-col gap-4 px-3 lg:flex-row lg:gap-10 lg:pr-0 lg:pl-15">
+        <div className="flex h-full flex-1 flex-col gap-4">
+          <div className="mx-auto flex items-center gap-5 lg:mx-0">
+            <div className="relative shrink-0">
+              <img
+                src={icon}
+                alt={title}
+                className="h-20 w-17.5 lg:h-25 lg:w-25"
+              />
+              <PlatformIcon
+                className="absolute -bottom-5 left-1/2 -translate-x-1/2 opacity-75"
+                aria-hidden="true"
+              />
+            </div>
+            <h3 className="max-w-47 text-center text-xl font-bold lg:max-w-none lg:text-start lg:text-3xl xl:text-[40px]">
+              {title}
+            </h3>
+          </div>
+          <p className="font-frutiger text-center text-sm italic lg:text-start lg:text-xl">
+            {description}
+          </p>
+        </div>
+        <div className="h-px w-full shrink-0 bg-slate-800 px-3 lg:h-[85%] lg:w-px lg:self-center lg:px-0" />
+        <div className="flex flex-1 flex-col justify-center gap-1.5 px-1 lg:justify-evenly lg:gap-2 lg:px-10 lg:py-1">
+          {details.map((detail, index) => (
+            <div className="flex items-start gap-2.5 lg:gap-6.5" key={index}>
+              <DetailsIcon
+                className="mt-1 hidden shrink-0 lg:block"
+                aria-hidden="true"
+              />
+              <CubeIcon className="mt-1 block lg:hidden" aria-hidden="true" />
+              <p key={index} className="text-sm font-medium lg:text-xl">
+                {detail}
+              </p>
+            </div>
+          ))}
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default SectorCard;
---
+++ src/components/which-sectors-do-we-serve-section/which-sectors-do-we-serve-section.tsx
@@ -0,0 +1,26 @@
+import FadeInWrapper from "../fade-in-wrapper";
+import SectionTitleAnimation from "../section-title-animation";
+import SectorCard from "./sector-card";
+import { sectorCardData } from "./sector-card-data";
+
+const WhichSectorsDoWeServeSection = () => {
+  return (
+    <div className="space-y-12 lg:space-y-32">
+      <SectionTitleAnimation text="Which Sectors Do We Serve?" />
+      <FadeInWrapper className="grid gap-9 sm:grid-cols-2 lg:grid-cols-1 lg:gap-15">
+        {sectorCardData.map(sector => (
+          <SectorCard
+            key={sector.id}
+            title={sector.title}
+            bgIcon={sector.backgroundIcon}
+            description={sector.description}
+            icon={sector.icon}
+            details={sector.details}
+          />
+        ))}
+      </FadeInWrapper>
+    </div>
+  );
+};
+
+export default WhichSectorsDoWeServeSection;
