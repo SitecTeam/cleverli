f2900205871da0a7a8cceed25e03ea9328bf53e1
Andrej Jurisic <andrejjurisic99@gmail.com>
2026-01-20 11:37:24 +0100

    fix console errors

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -2,18 +2,22 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
-function Input({ className, type, ...props }: React.ComponentProps<"input">) {
-  return (
-    <input
-      type={type}
-      data-slot="input"
-      className={cn(
-        "flex h-auto w-full rounded-none border-0 border-b border-white bg-transparent px-0 py-2 text-base text-white shadow-sm transition-colors selection:bg-primary selection:text-primary-foreground file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-[#8596AB] focus-visible:border-orange-400 focus-visible:ring-0 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        className
-      )}
-      {...props}
-    />
-  );
-}
+const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        data-slot="input"
+        className={cn(
+          "flex h-auto w-full rounded-none border-0 border-b border-white bg-transparent px-0 py-2 text-base text-white shadow-sm transition-colors selection:bg-primary selection:text-primary-foreground file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-[#8596AB] focus-visible:border-orange-400 focus-visible:ring-0 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Input.displayName = "Input";
 
 export { Input };
