6434df91a2f2bd1df2bf8337761bf5d659b50cbd
Andrej Jurisic <andrejjurisic99@gmail.com>
2026-01-23 13:08:11 +0100

    ....

--- src/components/get-in-touch-section/get-in-touch-form.tsx
+++ src/components/get-in-touch-section/get-in-touch-form.tsx
@@ -1,7 +1,6 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
-import { CalendarDays, Clock } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
   Form,
