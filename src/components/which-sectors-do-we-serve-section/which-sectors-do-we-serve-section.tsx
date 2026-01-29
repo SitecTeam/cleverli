d35e5106dd90d80bcce44296a84105db7bdced91
AndrejJurisic <124591321+AndrejJurisic@users.noreply.github.com>
2026-01-29 16:24:04 +0100

    CL-82 / Schedule a free consultation section (#30)
    * add svgs
    
    * add Book Your Call form component
    
    * add Schedule a Free Consultation section component
    
    * refactor Logo component: simplify imports, update navigation logic, and enhance accessibility features
    
    * refactor DarkPanel component: improve layout structure and enhance readability
    
    * update sector page
    
    * fix image not showing

--- src/components/which-sectors-do-we-serve-section/which-sectors-do-we-serve-section.tsx
+++ src/components/which-sectors-do-we-serve-section/which-sectors-do-we-serve-section.tsx
@@ -5,9 +5,9 @@ import { sectorCardData } from "./sector-card-data";
 
 const WhichSectorsDoWeServeSection = () => {
   return (
-    <div className="space-y-12 lg:space-y-32">
+    <div className="space-y-12 lg:space-y-16">
       <SectionTitleAnimation text="Which Sectors Do We Serve?" />
-      <FadeInWrapper className="grid gap-9 sm:grid-cols-2 lg:grid-cols-1 lg:gap-15">
+      <FadeInWrapper className="grid gap-9 sm:grid-cols-2 lg:grid-cols-1 lg:justify-center lg:gap-8 xl:gap-13.5">
         {sectorCardData.map(sector => (
           <SectorCard
             key={sector.id}
