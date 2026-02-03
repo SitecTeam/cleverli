5fd818eb1d2cc768752c09d988e72e7cd58b4b0b
Djordje Blagojevic <dj.blagojevic@gmail.com>
2026-02-03 09:18:13 +0100

    Refactor SVG backgrounds to use FadeInWrapper for improved animations and remove unnecessary filters for cleaner rendering

--- src/components/page-background.tsx
+++ src/components/page-background.tsx
@@ -3,12 +3,13 @@ import cubesMobile1Url from "@/svgs/cubes/cubes-mobile-1.svg?url";
 import cubesMobile2Url from "@/svgs/cubes/cubes-mobile-2.svg?url";
 import cubesMobile3Url from "@/svgs/cubes/cubes-mobile-3.svg?url";
 import cubesMobile4Url from "@/svgs/cubes/cubes-mobile-4.svg?url";
+import FadeInWrapper from "./fade-in-wrapper";
 
 const PageBackground = () => {
   return (
     <>
       {/* Mobile Background (< md) */}
-      <div className="pointer-events-none absolute inset-0 -top-40 -z-10 flex flex-col items-center justify-start overflow-hidden md:hidden">
+      <FadeInWrapper className="pointer-events-none absolute inset-0 -top-40 -z-10 flex flex-col items-center justify-start overflow-hidden md:hidden">
         <img
           src={cubesMobile1Url}
           alt=""
@@ -29,22 +30,22 @@ const PageBackground = () => {
           alt=""
           className="block w-full max-w-none min-w-150"
         />
-      </div>
+      </FadeInWrapper>
 
       {/* Tablet background (md to lg) - Full width */}
-      <div className="pointer-events-none absolute top-0 left-0 -z-10 hidden w-full justify-center md:flex lg:hidden">
+      <FadeInWrapper className="pointer-events-none absolute top-0 left-0 -z-10 hidden w-full justify-center md:flex lg:hidden">
         <img src={cubesMainLeftUrl} alt="" className="w-full" />
-      </div>
+      </FadeInWrapper>
 
       {/* Desktop background (lg+) - 1440px layout aligned */}
       <div className="pointer-events-none absolute top-0 left-0 -z-10 hidden w-full justify-center lg:flex">
-        <div className="relative w-full max-w-360">
+        <FadeInWrapper className="relative w-full max-w-360" margin="0px">
           <img
             src={cubesMainLeftUrl}
             alt=""
             className="absolute top-0 right-0 w-full max-w-none lg:w-360"
           />
-        </div>
+        </FadeInWrapper>
       </div>
     </>
   );
