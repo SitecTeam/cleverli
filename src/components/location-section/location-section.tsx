3bd4020845289b228aa702cd3ec48cc078598efd
Djordje Blagojevic <dj.blagojevic@gmail.com>
2026-02-04 08:23:10 +0100

    Refactor LocationSection layout and remove unused SVG import

--- src/components/location-section/location-section.tsx
+++ src/components/location-section/location-section.tsx
@@ -1,5 +1,4 @@
 import FadeInWrapper from "../fade-in-wrapper";
-import cubesUrl from "../../svgs/cubes/cubes-left.svg?url";
 
 const LocationSection = () => {
   return (
@@ -7,12 +6,6 @@ const LocationSection = () => {
       <FadeInWrapper className="mx-auto flex w-full max-w-360 flex-col gap-10 lg:flex-row lg:items-center lg:justify-between lg:px-0">
         {/* Left Content */}
         <div className="relative z-10 flex flex-col gap-6 pl-4 lg:w-full lg:pl-13.5">
-          <img
-            src={cubesUrl}
-            alt=""
-            className="absolute -top-20 -left-20 -z-10 h-64 w-64 opacity-60"
-          />
-
           <div className="relative w-full max-w-195.5 rounded-2xl bg-white/10 p-6 shadow-lg backdrop-blur-xs lg:pt-10.25 lg:pr-4 lg:pb-14.5 lg:pl-7.5">
             <h2 className="font-frutiger mb-6 text-3xl font-semibold text-slate-800 lg:text-[64px]">
               Where Are We Located?
